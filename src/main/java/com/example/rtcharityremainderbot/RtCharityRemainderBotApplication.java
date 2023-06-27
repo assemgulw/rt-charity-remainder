33650d706d081427cedacfa4d40cc57a788230fd
asemgul <sukhambekovaa@gmail.com>
2023-06-28 00:39:38 +0600

    initial commit

---
+++ src/main/java/com/example/rtcharityremainderbot/RtCharityRemainderBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.rtcharityremainderbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtCharityRemainderBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtCharityRemainderBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rtcharityremainderbot/RtCharityRemainderBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rtcharityremainderbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtCharityRemainderBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
